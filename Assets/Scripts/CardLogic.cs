c4a15e7063997f2852ed1bdf5d2479023ecbb3d5
pmilowski <pmilowski16@gmail.com>
2023-02-26 00:18:19 +0100

    Stress and Cash working

--- Assets/Scripts/CardLogic.cs
+++ Assets/Scripts/CardLogic.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -25,13 +26,13 @@ public class CardLogic : MonoBehaviour
     public Slider MarkSlider;
 
     public SpriteRenderer StressSprite;
+    public SpriteRenderer CashSprite;
 
     // Between 0 and 100
-    public int[] Stats = new int[4] { 50, 50, 50, 50 };
+    public int[] Stats = new int[5] { 50, 50, 50, 50, 20 };
 
     private Vector2 pos;
     private List<int> availableNums = new List<int>();
-
 
     public void OnMouseDrag(BaseEventData data)
     {
@@ -59,7 +60,6 @@ public class CardLogic : MonoBehaviour
             if (pos.x < -500f) UpdateStatsDisplay(UpdateStatsLeft(CardData));
             if (pos.x > 500f) UpdateStatsDisplay(UpdateStatsRight(CardData));
 
-            // UpdateStressDisplay(UpdateStress());
             UpdateStressDisplay(UpdateStress());
             LoadCardData(CardData);
         }
@@ -109,17 +109,47 @@ public class CardLogic : MonoBehaviour
     {
         for (int i = 0; i < Stats.Length; i++)
         {
-            // Stats[i] += CardValues.rightStats[i];
+            Stats[i] += CardValues.rightStats[i];
         }
         return Stats;
     }
 
     public void UpdateStatsDisplay(int[] stats)
     {
-        ITSlider.value = Stats[0];
-        HRSlider.value = Stats[1];
-        AccSlider.value = Stats[2];
-        MarkSlider.value = Stats[3];
+        ITSlider.value = stats[0];
+        HRSlider.value = stats[1];
+        AccSlider.value = stats[2];
+        MarkSlider.value = stats[3];
+
+        Sprite cashSprite;
+
+        switch (stats[4])
+        {
+            case 20:
+                cashSprite = Resources.Load<Sprite>("CashSprites/pasek_20_kasa");
+                break;
+
+            case 40:
+                cashSprite = Resources.Load<Sprite>("CashSprites/pasek_40_kasa");
+                break;
+
+            case 60:
+                cashSprite = Resources.Load<Sprite>("CashSprites/pasek_60_kasa");
+                break;
+
+            case 80:
+                cashSprite = Resources.Load<Sprite>("CashSprites/pasek_80_kasa");
+                break;
+
+            case 100:
+                cashSprite = Resources.Load<Sprite>("CashSprites/pasek_100_kasa");
+                break;
+
+            default:
+                cashSprite = Resources.Load<Sprite>("CashSprites/pasek_20_kasa");
+                break;
+        }
+        CashSprite.sprite = cashSprite;
     }
 
     public int UpdateStress()
@@ -140,34 +170,34 @@ public class CardLogic : MonoBehaviour
     public void UpdateStressDisplay(int stress)
     {
         Sprite newSprite;
-        Debug.Log("Stress: " + stress);
+        // Debug.Log("Stress: " + stress);
         switch (stress)
         {
             case 20:
-                newSprite = Resources.Load("StressSprites/pasek_20_stres") as Sprite;
+                newSprite = Resources.Load<Sprite>("StressSprites/pasek_20_stres");
                 break;
 
             case 40:
-                newSprite = Resources.Load("StressSprites/pasek_40_stres") as Sprite;
+                newSprite = Resources.Load<Sprite>("StressSprites/pasek_40_stres");
                 break;
 
             case 60:
-                newSprite = Resources.Load("StressSprites/pasek_60_stres") as Sprite;
+                newSprite = Resources.Load<Sprite>("StressSprites/pasek_60_stres");
                 break;
 
             case 80:
-                newSprite = Resources.Load("StressSprites/pasek_80_stres") as Sprite;
+                newSprite = Resources.Load<Sprite>("StressSprites/pasek_80_stres");
                 break;
 
             case 100:
-                newSprite = Resources.Load("StressSprites/pasek_100_stres") as Sprite;
+                newSprite = Resources.Load<Sprite>("StressSprites/pasek_100_stres");
                 break;
 
             default:
-                newSprite = Resources.Load("StressSprites/pasek_20_stres") as Sprite;
+                newSprite = Resources.Load<Sprite>("StressSprites/pasek_20_stres");
                 break;
         }
-        Debug.Log("Sprite name: " + newSprite.name);
+        // Debug.Log("Sprite name: " + newSprite.name);
         StressSprite.sprite = newSprite;
     }
 
